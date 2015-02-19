6ef5e4375f9c99864dffb9ce5ffb5b244840568b
my-get-dodo <thomas.lecorre@epitech.eu>
2015-02-19 17:20:03 +0100

    fragments content

--- app/src/main/java/com/epitech/cloare_q/epiandroid/home.java
+++ app/src/main/java/com/epitech/cloare_q/epiandroid/home.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,19 +41,95 @@ public class home extends Activity {
         }
     }
 
+    //LES FRAGMENTS
+
+    public static class FragHome extends Fragment {
+
+        public FragHome() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.fragment_home, container, false);
+        }
+    }
+
+    public static class FragPlanning extends Fragment {
+
+        public FragPlanning() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.fragment_planning, container, false);
+        }
+    }
+
+    public static class FragToken extends Fragment {
+
+        public FragToken() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.fragment_token, container, false);
+        }
+    }
+
+    public static class FragGrades extends Fragment {
+
+        public FragGrades() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.fragment_grades, container, false);
+        }
+    }
+
+
+
     /** Swaps fragments in the main content view */
     private void selectItem(int position) {
         // Create a new fragment and specify the planet to show based on position
-        Fragment fragment = new EpiFrag();
-        Bundle args = new Bundle();
-        args.putInt(EpiFrag.ARG_MENU_NUMBER, position);
-        fragment.setArguments(args);
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new FragHome();
+                break;
+            case 1:
+                fragment = new FragPlanning();
+                break;
+            case 2:
+                fragment = new FragToken();
+                break;
+            case 3 :
+                fragment = new FragGrades();
+                break;
+        }
+        //Fragment fragment = new EpiFrag();
+        //Bundle args = new Bundle();
+        //args.putInt(EpiFrag.ARG_MENU_NUMBER, position);
+        //fragment.setArguments(args);
 
         // Insert the fragment by replacing any existing fragment
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.content_frame, fragment)
-                .commit();
+            if (fragment != null) {
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_frame, fragment)
+                        .commit();
+            } else {
+                // error in creating fragment
+                Log.e("Home Activity", "Error in creating fragment");
+            }
 
         // Highlight the selected item, update the title, and close the drawer
         mDrawerList.setItemChecked(position, true);
@@ -60,22 +137,5 @@ public class home extends Activity {
         mDrawerLayout.closeDrawer(mDrawerList);
     }
 
-    public static class EpiFrag extends Fragment {
-        public static final String ARG_MENU_NUMBER = "menu_number";
 
-        public EpiFrag() {
-            // Empty constructor required for fragment subclasses
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.epi_fragment, container, false);
-            int i = getArguments().getInt(ARG_MENU_NUMBER);
-            String menuItem = getResources().getStringArray(R.array.menu_array)[i];
-
-            getActivity().setTitle(menuItem);
-            return rootView;
-        }
-    }
 }
