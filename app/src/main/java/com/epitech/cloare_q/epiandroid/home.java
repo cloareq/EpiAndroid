a3454c5ef57e582cde75ed80200af72de878856d
my-get-dodo <thomas.lecorre@epitech.eu>
2015-02-19 15:26:29 +0100

    drawer menu (tirer l'ecran sur le bord gauche) manque chargement du contenu

--- app/src/main/java/com/epitech/cloare_q/epiandroid/home.java
+++ app/src/main/java/com/epitech/cloare_q/epiandroid/home.java
@@ -1,34 +1,81 @@
 package com.epitech.cloare_q.epiandroid;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-/**
- *
- * Home Screen Activity
- */
 public class home extends Activity {
-    final String EXTRA_TOKEN = "user_token";
+    private String[] menuList;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
-        Intent intent = getIntent();
-        TextView tokenDisplay = (TextView) findViewById(R.id.showToken);
+        menuList = getResources().getStringArray(R.array.menu_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        if (intent != null) {
-            tokenDisplay.setText(intent.getStringExtra(EXTRA_TOKEN));
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, menuList));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
         }
     }
 
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+        Fragment fragment = new EpiFrag();
+        Bundle args = new Bundle();
+        args.putInt(EpiFrag.ARG_MENU_NUMBER, position);
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(menuList[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    public static class EpiFrag extends Fragment {
+        public static final String ARG_MENU_NUMBER = "menu_number";
+
+        public EpiFrag() {
+            // Empty constructor required for fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.epi_fragment, container, false);
+            int i = getArguments().getInt(ARG_MENU_NUMBER);
+            String menuItem = getResources().getStringArray(R.array.menu_array)[i];
+
+            getActivity().setTitle(menuItem);
+            return rootView;
+        }
+    }
 }
